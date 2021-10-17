8a0650bd705478816a7fc7ad146949d0f69a557d
Bruno Bennotti B Moura <brunobennotti@gmail.com>
2021-10-17 13:59:52 -0300

    init

---
+++ devops/redis-server/run-redis.ps1
@@ -0,0 +1,18 @@
+$projectName=$args[0]
+$ambiente=$args[1]
+write-host "Parametros defaults, Sintaxe customizada: run-redis.ps1 projectName local|dev|tst|hml|prd" 
+write-host "Parametros defaults, default: run-redis.ps1 projectName local" 
+
+if ($projectName -eq $null) {
+    $projectName = "projectName"
+}
+
+if ($ambiente -eq $null) {
+    $ambiente = "local"
+}
+
+$dockerProjectName=$projectName + '_redis_' + $ambiente
+$dockerFileName='docker-comp-redis-' + $ambiente + '.yml'
+
+docker-compose -p $dockerProjectName -f ./devops/redis-server/$dockerFileName down
+docker-compose -p $dockerProjectName -f ./devops/redis-server/$dockerFileName up --build -d
---
+++ src/Program.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace poc_csharp_console_app_redis
+{
+    class Program
+    {
+        private const string RedisConnectionString = "localhost,port: 6379,password=123456";
+        private static ConnectionMultiplexer connection =
+          ConnectionMultiplexer.Connect(RedisConnectionString);
+
+        static void Main(string[] args)
+        {
+            IDatabase db = connection.GetDatabase();
+            db.StringSet("mykey", DateTime.Now.ToString());
+
+            Console.WriteLine(db.StringGet("mykey"));
+            Console.WriteLine(db.StringGet("key-add-on-another-client"));
+            Console.WriteLine(db.StringGet("key-404"));
+        }
+    }
+}
